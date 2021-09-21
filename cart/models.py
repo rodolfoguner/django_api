4e8bd1dd71ae472e7eafa5b3fba291345de1eb5b
Rodolfo Mamud <rodolfo.guner@outlook.com>
2021-09-20 21:15:31 -0300

    cart_product finalizer

--- cart/models.py
+++ cart/models.py
@@ -3,14 +3,8 @@ from django.db.models.fields import FloatField
 from client.models import Client
 from product.models import Product
 
+
 class Cart(models.Model):
     owner = models.ForeignKey(Client, on_delete=models.CASCADE)
     total = FloatField()
     product = models.ManyToManyField(Product)
-
-
-
-
-
-
-
--- cart/views.py
+++ cart/views.py
@@ -6,4 +6,3 @@ from .models import Cart
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
-
